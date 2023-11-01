264f031a437e916c3b535f0196d5463356aa8dec
Soft-Mahmood <softmahmood@gmail.com>
2023-11-01 11:32:15 -0700

    betty

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -8,30 +8,30 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-        struct binary_tree_s *tmp, *new;
+	struct binary_tree_s *tmp, *new;
 
-        if (parent == NULL)
-                return (NULL);
-        if (parent->right == NULL)
-        {
-                parent->right = binary_tree_node(parent, value);
-                return (parent->right);
-        }
-        else
-        {
-                tmp = malloc(sizeof(struct binary_tree_s));
-                tmp->n = parent->right->n;
-                tmp->left = parent->right->left;
-                tmp->right = parent->right->right;
-                tmp->parent = NULL;
+	if (parent == NULL)
+		return (NULL);
+	if (parent->right == NULL)
+	{
+		parent->right = binary_tree_node(parent, value);
+		return (parent->right);
+	}
+	else
+	{
+		tmp = malloc(sizeof(struct binary_tree_s));
+		tmp->n = parent->right->n;
+		tmp->left = parent->right->left;
+		tmp->right = parent->right->right;
+		tmp->parent = NULL;
 
-                new = malloc(sizeof(struct binary_tree_s));
-                new = binary_tree_node(parent, value);
-                parent->right = new;
-                new->right = tmp;
-                tmp->parent = new;
+		new = malloc(sizeof(struct binary_tree_s));
+		new = binary_tree_node(parent, value);
+		parent->right = new;
+		new->right = tmp;
+		tmp->parent = new;
 
-                return (new);
-                ;
-        };
+		return (new);
+		;
+	};
 }
